bb3d6f3fa500a21dddee8fce594c9c375f6ff462
Killeroo <matthewcarney64@gmail.com>
2018-08-08 01:53:42 +0100

    Added basic structure of program Added small logger class

---
+++ Spudule/Logger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spudule
+{
+    /// <summary>
+    /// Constructs and displays log messages to standard output
+    /// </summary>
+    public class Logger
+    {
+        public string Identifier { get; set; } = "Spudule";
+        private ConsoleColor startFrontColor;
+        private ConsoleColor startBackColor;
+
+        public Logger()
+        {
+            startFrontColor = Console.ForegroundColor;
+            startBackColor = Console.BackgroundColor;
+        }
+
+        public Logger(string identifier)
+        {
+            Identifier = identifier;
+            startFrontColor = Console.ForegroundColor;
+            startBackColor = Console.BackgroundColor;
+        }
+
+        public void Debug(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; ;
+            Console.WriteLine($"[{DateTime.Now}] [{Identifier}] [Debug] " + message);
+            ResetColor();
+        }
+        public void Info(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"[{DateTime.Now}] [{Identifier}] [Info] " + message);
+            ResetColor();
+        }
+        public void Warning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[{DateTime.Now}] [{Identifier}] [Warning] " + message);
+            ResetColor();
+        }
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[{DateTime.Now}] [{Identifier}] [Error] " + message);
+            ResetColor();
+        }
+        public void Critical(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"[{DateTime.Now}] [{Identifier}] [Critical] " + message);
+            ResetColor();
+        }
+
+        private void ResetColor()
+        {
+            Console.ForegroundColor = startFrontColor;
+            Console.BackgroundColor = startBackColor;
+        }
+    }
+}
--- Spudule/Program.cs
+++ Spudule/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Discord;
 using Discord.Webhook;
 using FluentScheduler;
+using System.Linq;
 
 // Need:
 // Logging
@@ -16,23 +20,53 @@ namespace Spudule
 {
     class Program
     {
+        // Holds event data in following format (time eg 16.00), (user), (message)
+        private static List<Tuple<string, string, string>> events;
+
         private static DiscordWebhookClient hook;
-        private static Timer timer;
+        private static List<Embed> embeds = new List<Embed>();
+        private static Logger logger = new Logger();
+        private static ulong id = 0;
+        private static string token;
 
         static void Main(string[] args)
         {
-            hook = new DiscordWebhookClient();
-            var embed = new EmbedBuilder()
-                .WithTitle("Test")
-                .WithImageUrl("https://pbs.twimg.com/profile_images/559119938687107072/i6Ox_iOk_400x400.jpeg").Build();
+            // Try read config file and load values
+            events = ReadConfigFile("config.txt");
 
+            // Sanity check some values
+            if (String.IsNullOrEmpty(token) || id == 0 || events.Count == 0) {
+                logger.Critical("Problem reading config file, cannot find an event, token or id");
+                Console.WriteLine("config.txt should be formatted with the following:\n" +
+                    "\n\ttoken asd23emk2ek12nmk2l1mn2\n" +
+                    "\tid 21312535523532\n" +
+                    "\tevent 16:00 killeroo \"meow meow meow\"\n");
+            }
+
+            // Setup embeds for each event
+            var emoji = new Emoji(":loudspeaker:");
+            foreach (var e in events) {
+                var num = MentionUtils.ParseUser("@" + e.Item2);
+                // TODO: Trying to get user mention
+                embeds.Add(new EmbedBuilder()
+                .WithTitle($"{emoji} Reminder")
+                .WithDescription($" {e.Item3}").Build());
+            }
+
+            // Setup hook
+            hook = new DiscordWebhookClient(id, token);
+            logger.Info("Discord Webhook created");
+
+            //TODO: Comment more of this 
             var registry = new Registry();
             registry.Schedule(() =>
             {
-                hook.SendMessageAsync("timer eevent", false, null, "Spudule", "http://www.dutchdc.com/wp-content/uploads/2016/12/Potato_shadow.png");
+                hook.SendMessageAsync("", false, new Embed[] { embeds[0] }, "Spudule", "http://www.dutchdc.com/wp-content/uploads/2016/12/Potato_shadow.png");
             }).ToRunNow().AndEvery(1).Minutes();
 
             JobManager.Initialize(registry);
+
+            //hook.SendMessageAsync("test test", false, new Embed[] { embed }, "Spudule", "http://www.dutchdc.com/wp-content/uploads/2016/12/Potato_shadow.png");
 
             Console.ReadLine();
             //while (true) {
@@ -41,6 +75,49 @@ namespace Spudule
             //    Console.ReadLine();
             //}
 
+        }
+
+        private static List<Tuple<string, string, string>> ReadConfigFile(string path)
+        {
+            List<Tuple<string, string, string>> events = new List<Tuple<string, string, string>>();
+
+            try {
+                // Open config file (create if not found)
+                var lines = File.ReadAllLines(path);
+                foreach (var line in lines) {
+                    var words = line.Split(' ');
+                     
+                    // Process based on the first word of each line
+                    switch (words[0].ToLower()) {
+                        case "event": // Format example: event 16:00 killeroo "something here"
+                            var regex = new Regex("\"[^\"]*\"");
+                            var matches = regex.Matches(line).Cast<Match>().Select(m => m.Value).ToArray(); // Find strings inbetween quotation marks and save as message
+                            events.Add(new Tuple<string, string, string>(words[1], words[2], matches[0]));
+                            logger.Debug($"Added event: time={words[1]} username={words[2]} message={matches[0]}");
+                            break;
+                        case "token": // Format example: token kn21k32knk2n32kn
+                            token = words[1];
+                            logger.Debug($"Token: {words[1]}");
+                            break;
+                        case "id": // Format example: id 2312315322
+                            // Convert without loss of precision
+                            decimal d = Decimal.Parse(words[1]);
+                            ulong u = (ulong)d;
+                            id = u;
+                            logger.Debug($"ID detected: {words[1]}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            } catch (Exception e) {
+                logger.Critical($"type={e.GetType().ToString()} msg=\"{e.Message}\"");
+                Environment.Exit(1);
+            }
+
+             logger.Info("Config file read");
+            return events;
         }
 
         private static Task TimerEvent()
